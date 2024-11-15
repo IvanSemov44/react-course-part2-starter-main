547bc851be27300b4fe919fa16e3705f82602104
IvanSemov44 <ivan.semov.44@gmail.com>
2024-11-15 10:49:56 +0200

    implementing pagination

--- src/hooks/usePosts.ts
+++ src/hooks/usePosts.ts
@@ -8,14 +8,23 @@ interface Post {
   userId: number;
 }
 
-export default (userId: number | undefined) =>
+interface PostQuery {
+  page: number;
+  pageSize: number;
+}
+
+export default (query: PostQuery) =>
   useQuery<Post[], Error>({
-    queryKey: userId ? ["users", userId, "posts"] : ["posts"],
+    queryKey: ["post", query],
     queryFn: () =>
       axios
         .get("https://jsonplaceholder.typicode.com/posts", {
-          params: { userId },
+          params: {
+            _start: (query.page - 1) * query.pageSize,
+            _limit: query.pageSize,
+          },
         })
         .then((res) => res.data),
     staleTime: 10 * 1000,
+    keepPreviousData: true,
   });
--- src/react-query/PostList.tsx
+++ src/react-query/PostList.tsx
@@ -2,8 +2,12 @@ import { useState } from 'react';
 import usePosts from '../hooks/usePosts';
 
 const PostList = () => {
-  const [userId, setUserId] = useState<number>();
-  const { data: posts, error, isLoading } = usePosts(userId);
+  const pageSize = 10;
+  const [page, setPage] = useState(1);
+  // const [userId, setUserId] = useState<number>();
+  const { data: posts, error, isLoading } = usePosts({ page, pageSize }
+    // userId
+  );
 
   if (isLoading) return <p>"Loading"</p>;
 
@@ -11,7 +15,7 @@ const PostList = () => {
 
   return (
     <>
-      <select
+      {/* <select
         className="form-select md-3"
         value={userId}
         onChange={(event) => setUserId(parseInt(event.target.value))}
@@ -20,7 +24,7 @@ const PostList = () => {
         <option value="1">User 1</option>
         <option value="2">User 2</option>
         <option value="3">User 3</option>
-      </select>
+      </select> */}
       <ul className="list-group">
         {posts?.map((post) => (
           <li key={post.id} className="list-group-item">
@@ -28,6 +32,19 @@ const PostList = () => {
           </li>
         ))}
       </ul>
+      <button
+        disabled={page === 1}
+        className="btn btn-primary my-3"
+        onClick={() => setPage(page - 1)}
+      >
+        Previous
+      </button>
+      <button
+        className="btn btn-primary my-3 ms-3"
+        onClick={() => setPage(page + 1)}
+      >
+        Next
+      </button>
     </>
   );
 };
