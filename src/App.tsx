8b830f9dbc84bded9600858a3702862be0129305
IvanSemov44 <ivan.semov.44@gmail.com>
2024-11-17 18:05:54 +0200

    state logic with reducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import TodoForm from "./react-query/TodoForm";
-import TodoList from "./react-query/TodoList";
+import Counter from "./state-management/Counter";
 
 function App() {
   return (
     <>
-      <TodoForm />
-      <TodoList />
+      <Counter />
+      {/* <TodoForm />
+      <TodoList /> */}
 
       {/* <PostList /> */}
     </>
